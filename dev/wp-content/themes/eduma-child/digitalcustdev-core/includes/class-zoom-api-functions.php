8cea4bd02f84bd53dbbbee6b6f4cb4dc80169008
ronymaha <ronymaha@gmail.com>
2020-05-10 16:36:09 +0600

    update

--- dev/wp-content/themes/eduma-child/digitalcustdev-core/includes/class-zoom-api-functions.php
+++ dev/wp-content/themes/eduma-child/digitalcustdev-core/includes/class-zoom-api-functions.php
@@ -215,6 +215,41 @@ if ( ! class_exists( 'DigitalCustDev_Zoom_API' ) && class_exists('Zoom_Video_Con
 		}
 
 
+		/*
+		* Get Single Webinar Details
+		 @Source: https://marketplace.zoom.us/docs/api-reference/zoom-api/webinars/webinar
+		*/
+		public function getZoomWebinarDetails($post_id){
+			$webinarId = get_post_meta( $post_id, '_webinar_ID', true );
+			if($webinarId):
+				$curl = curl_init();
+				curl_setopt_array($curl, array(
+				CURLOPT_URL => "https://api.zoom.us/v2/webinars/" . $webinarId,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => "",
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "GET",
+				CURLOPT_HTTPHEADER => array(
+					"authorization: Bearer ".$this->generateJWTKey().""
+				),
+				));
+
+				$response = curl_exec($curl);
+				$err = curl_error($curl);
+
+				curl_close($curl);
+
+				if ($err) {
+				return "cURL Error #:" . $err;
+				} else {
+				return $response;
+				}
+			endif;
+		}
+
+
 
 
 		function add_roles( $roles ) {
